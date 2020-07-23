03fcb362336e0f415405cd1e7c58e7511281cbe1
hari <nepal9841921783@gmail.com>
2020-07-23 14:39:16 +0100

    test1

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React, {Fragment} from 'react';
 
 import Navbar from "./Components/layout/Navbar";
 import './App.css'
-import './bootstrap.css'
 import DarkState from "./Context/darkMode/DarkState";
 import Footer from "./Components/layout/Footer";
 import ContactState from "./Context/contact/ContactState";
